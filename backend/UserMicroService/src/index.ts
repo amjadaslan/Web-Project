ed58f68e546d9c5e4c97c61c7d3c2f3a279f663c
Amjad Aslan <amjad_aslan@Hotmail.com>
2023-04-30 17:58:30 +0300

    fixing logout

--- backend/UserMicroService/src/index.ts
+++ backend/UserMicroService/src/index.ts
@@ -151,7 +151,11 @@ app.get('/api/user/:userId/username', function (req: RequestWithUserInfo, res) {
 
 app.post('/api/user/logout', function (req: RequestWithUserInfo, res) {
   try {
-    res.clearCookie('token', {expires: new Date(1), path: '/' });
+    res.cookie('token', 'invalidToken', {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
   } catch (err) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
     res.end();
